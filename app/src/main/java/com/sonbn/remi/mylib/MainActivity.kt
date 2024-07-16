b27a24f8182e2f74a49c48859c9d7fd96d8a03f1
Remi Design 1 <remidesign1@192.168.102.30>
2024-07-16 13:47:41 +0700

    fix

--- app/src/main/java/com/sonbn/remi/mylib/MainActivity.kt
+++ app/src/main/java/com/sonbn/remi/mylib/MainActivity.kt
@@ -3,10 +3,13 @@ package com.sonbn.remi.mylib
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sonbn.remi.mylib.ads.AdBanner
+import com.sonbn.remi.mylib.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.android.library") version "8.1.3" apply false
 }
